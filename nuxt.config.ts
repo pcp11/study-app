c8afb5b94737ce6722cfaa1b472d76aa91332427
pcp11 <patrick.christ@protonmail.com>
2025-05-21 21:43:09 +0200

    setup git pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@tresjs/nuxt']
+  modules: ['@tresjs/nuxt'],
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/' // Crucial for GitHub Pages deployment
+  },
+  ssr: false // Recommended for fully static deployment on GitHub Pages
 })
